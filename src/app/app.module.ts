babc0b9da17166adc2071af069b9c6c1f8de9c41
jayprajapati95 <35921472+jayprajapati95@users.noreply.github.com>
2018-05-16 17:56:50 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { DemoServiceService } from './demo-service.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DemoMapAirPollutionMapComponent } from './demo-map-air-pollution-map/demo-map-air-pollution-map.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoMapAirPollutionMapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    DemoServiceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
